5fcc51d45c5ba6fd2cf1d3bff68b2e877d974e64
Valera Sokolov <komproekt42@gmail.com>
2022-10-20 21:16:26 +0300

    Ver1

--- 2.php
+++ 2.php
@@ -3,6 +3,6 @@
 header('Content-Type: text/plain');
 header('Content-Disposition:attachment; filename="downloaded.txt"');
 
-$c = $_GET['a'];
+$c = $_GET['text'];
 
 echo $c;
--- index.php
+++ index.php
@@ -6,11 +6,11 @@ declare(strict_types=1);
 //var_dump($_SERVER);
 //header($_SERVER["SERVER_PROTOCOL"].' 404 Not Found');
 
-$text = 'Я переданный параметр в GET запросе';
+$text = 'Я переданный параметр text в GET запросе';
 
 echo '<ul> Список страниц';
 echo '<li> <a href="1.php">Первая страница </a></li>';
-echo '<li> <a href="2.php/?a=' . $text . '">Вторая страница </a></li>';
+echo '<li> <a href="2.php/?text=' . $text . '">Вторая страница </a></li>';
 echo '<li> <a href="3.php">Третья страница </a></li>';
 echo '<li> <a href="4.php">Четвертая страница </a></li>';
 echo '</ul>';
